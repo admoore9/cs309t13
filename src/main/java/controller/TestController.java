e2a7790a4e462f55f61ba97087ae82d611537643
Brian Shannan <brianshannan@gmail.com>
2014-09-13 17:19:49 -0500

    more maven stuff, almost runs now

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,14 +1,14 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
     
     @RequestMapping("/test")
-    public String test(Model model) {
+    public String test(ModelMap model) {
         model.addAttribute("name", "this is some text I entered");
         return "test";
     }
