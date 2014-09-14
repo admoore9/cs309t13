a5552a0c9dadfc2b641533a732db335408e1a2e5
Brian Shannan <brianshannan@gmail.com>
2014-09-14 10:24:31 -0500

    a few formatting things

--- src/main/java/edu/iastate/controllers/TestController.java
+++ src/main/java/edu/iastate/controllers/TestController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/test")
 public class TestController {
-    
-    @RequestMapping(method=RequestMethod.GET)
+
+    @RequestMapping(method = RequestMethod.GET)
     public String test(Model model) {
         model.addAttribute("name", "something put in in the controller");
         return "test";
