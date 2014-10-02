ad6255eccd64a3bff94dc7f95b2ace5141c8ab4a
Andrew Moore <admoore9@gmail.com>
2014-10-02 16:46:22 -0500

    add getter and setter methods for game properties

--- src/main/java/edu/iastate/models/Game.java
+++ src/main/java/edu/iastate/models/Game.java
@@ -1,16 +1,27 @@
 package edu.iastate.models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+/**
+ * Game class
+ * 
+ * @author Andrew
+ *
+ */
 
 @Entity
 @Table(name = "Game")
@@ -28,14 +39,61 @@ public class Game {
     @Column(name = "game_location")
     private String gameLocation;
 
-    // private Game nextGame;
-    // private List<Team> teams;
-
+//    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "game")
+//    private String nextGame;
+//    
+//    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "game")
+//    private List<Team> teams;
+    
     @ManyToOne
     @JoinColumn(name = "tournament_id")
     private Tournament tournament;
 
+    public int getId() {
+    	return id;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
+    public Date getGameTime() {
+    	return gameTime;
+    }
+    
+    public void setGameTime(Date gameTime) {
+    	this.gameTime = gameTime;
+    }
+    
     public String getGameLocation() {
-        return gameLocation;
+    	return gameLocation;
+    }
+    
+    public void setGameLocation(String gameLocation) {
+    	this.gameLocation = gameLocation;
+    }
+    
+//    public String getNextGame() {
+//    	return nextGame;
+//    }
+//    
+//    public void getNextGame(String nextGame) {
+//    	this.nextGame = nextGame;
+//    }
+//    
+//    public List<Team> getTeams() {
+//    	return teams;
+//    }
+//    
+//    public void setTeams(List<Team> teams) {
+//    	this.teams = teams;
+//    }
+    
+    public Tournament getTournament() {
+    	return tournament;
+    }
+    
+    public void setTournament(Tournament tournament) {
+    	this.tournament = tournament;
     }
 }
