c89090423438775fe587093bad8c495fd086671d
Nawaf Alsulami <nawafnaim@gmail.com>
2014-10-01 22:51:37 -0500

    Added initial methods and fields

--- src/main/java/edu/iastate/models/Player.java
+++ src/main/java/edu/iastate/models/Player.java
@@ -1,6 +1,35 @@
 package edu.iastate.models;
 
+import java.util.List;
 
-public class Player {
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+public class Player extends Member {
+	
+    @ManyToMany
+    @JoinColumn(name = "team_id")
+    private List<Team> teams;
+    
+    @ManyToMany
+    @JoinColumn(name = "game_id")
+    private List<Game> games;
+    
+    @Column(name = "free_agent")
+    private Boolean isFreeAgent;
+    
+    public List<Team> getTeams() {
+        return teams;
+    }
+    
+    public List<Game> getGames() {
+        return games;
+    }
+    
+    public Boolean isFreeAgent() {
+    	return isFreeAgent;
+    }
 
 }
