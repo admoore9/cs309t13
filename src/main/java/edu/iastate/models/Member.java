d06d8c5d641b7b6fed9ac0b91df55b80521bf82e
nawaf <nawafnaim@gmail.com>
2014-10-12 18:53:24 -0500

    added constructors and UserType field

--- src/main/java/edu/iastate/models/Member.java
+++ src/main/java/edu/iastate/models/Member.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import edu.iastate.models.Admin.View;
 
 /**
  * 
@@ -17,6 +21,24 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "Member")
 public class Member {
+
+    
+    public Member() {}
+
+    public Member(String name, String username, String password,
+            UserType userType) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.userType = userType;
+    }
+    
+    public Member(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.userType = UserType.PLAYER;
+    }
 
     @Id
     @GeneratedValue
@@ -38,20 +60,66 @@ public class Member {
     @Column(name = "is_official")
     private Boolean isOfficial;
     
-    public int getID() {
-    	return id;
+    public enum UserType {PLAYER, OFFICIAL, ADMIN}; 
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "user_type")
+    private UserType userType;
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
     }
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
-    	return name;
+        return name;
     }
-    
-    public Boolean isAdmin() {
-    	return isAdmin;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    
-    public Boolean isOfficial() {
-    	return isOfficial;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public Boolean getIsOfficial() {
+        return isOfficial;
+    }
+
+    public void setIsOfficial(Boolean isOfficial) {
+        this.isOfficial = isOfficial;
     }
     
 }
