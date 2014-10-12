c67c4d018539f1bac37ab32cb3a54c437942856c
nawaf <nawafnaim@gmail.com>
2014-10-12 18:52:34 -0500

    added constructors and current view field

--- src/main/java/edu/iastate/models/Admin.java
+++ src/main/java/edu/iastate/models/Admin.java
@@ -1,6 +1,45 @@
 package edu.iastate.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Admin")
 public class Admin extends Member {
 	
+    
+    
+    public Admin() {
+        super();
+    }
+
+    public Admin(String name, String username, String password) {
+        super(name, username, password, UserType.ADMIN);
+    }
+
+    public enum View {ADMIN, PLAYER, OFFICIAL}; 
+    
+    @JoinColumn(name = "member_id")
+    private int id;
+    
+    /**
+     * 
+     */
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "current_view")
+    private View curView;
+    
+    public View getCurView() {
+        return curView;
+    }
+    
+    public void setCurView(View newCurView) {
+        this.curView = newCurView;
+    }
 	
 }
