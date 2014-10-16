574f0d2d8648ed8d4141c91bc54514eab032565c
nawaf <nawafnaim@gmail.com>
2014-10-16 10:42:21 -0500

    removed id variable

--- src/main/java/edu/iastate/models/Admin.java
+++ src/main/java/edu/iastate/models/Admin.java
@@ -4,28 +4,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Admin")
 public class Admin extends Member {
 	
-    
-    
-    public Admin() {
-        super();
-    }
-
-    public Admin(String name, String username, String password) {
-        super(name, username, password, UserType.ADMIN);
-    }
+//    public Admin() {
+//        super();
+//    }
+//
+//    public Admin(String name, String username, String password) {
+//        super(name, username, password, UserType.ADMIN);
+//    }
 
     public enum View {ADMIN, PLAYER, OFFICIAL}; 
-    
-    @JoinColumn(name = "member_id")
-    private int id;
     
     /**
      * 
