901e32440a99b04a63028ba592560235a525e872
nawaf <nawafnaim@gmail.com>
2014-10-16 10:35:21 -0500

    add constructors and returnAllAdmins method

--- src/main/java/edu/iastate/dao/AdminDao.java
+++ src/main/java/edu/iastate/dao/AdminDao.java
@@ -1,5 +1,35 @@
 package edu.iastate.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import edu.iastate.models.Admin;
+
 public class AdminDao extends MemberDao {
     
+    public AdminDao() {
+        super();
+    }
+
+    public AdminDao(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
+    
+    public List<Admin> returnAllAdmins() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        TypedQuery<Admin> query = entityManager.createQuery("from Admin", Admin.class);
+        List<Admin> admins = query.getResultList();
+
+        transaction.commit();
+        entityManager.close();
+        
+        return admins;
+    }
 }
