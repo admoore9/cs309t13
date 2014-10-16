7cba3a6e9a490524fa092f329bec602c2137be43
nawaf <nawafnaim@gmail.com>
2014-10-16 10:38:33 -0500

    added constructors and returnAllPlayers method

--- src/main/java/edu/iastate/dao/PlayerDao.java
+++ src/main/java/edu/iastate/dao/PlayerDao.java
@@ -1,7 +1,35 @@
 package edu.iastate.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import edu.iastate.models.Player;
+
 public class PlayerDao extends MemberDao {
 	
-	
+    public PlayerDao() {
+        super();
+    }
 
+    public PlayerDao(EntityManagerFactory entityManagerFactory) {
+        super(entityManagerFactory);
+    }
+
+    public List<Player> returnAllPlayers() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        TypedQuery<Player> query = entityManager.createQuery("from Player", Player.class);
+        List<Player> players = query.getResultList();
+
+        transaction.commit();
+        entityManager.close();
+        
+        return players;
+    }
 }
