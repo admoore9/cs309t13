88e64e2de6bc70d399fe39b17223e03ac402a13d
nawaf <nawafnaim@gmail.com>
2014-10-18 23:05:49 -0500

    added test for getPlayerById

--- src/main/java/edu/iastate/tests/PlayerTests.java
+++ src/main/java/edu/iastate/tests/PlayerTests.java
@@ -2,6 +2,8 @@ package edu.iastate.tests;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import edu.iastate.dao.PlayerDao;
@@ -9,22 +11,38 @@ import edu.iastate.models.Player;
 
 public class PlayerTests {
 
-//    @Before
-//    public void setUp() throws Exception {
-//    }
-//
+    PlayerDao playerDao;
+    
+    @Before
+    public void setUp() throws Exception {
+        playerDao = new PlayerDao();
+//        playerDao.register("Sam", "sam", "123");
+    }
+
 //    @After
 //    public void tearDown() throws Exception {
+//        playerDao.deletePlayerById("7");
+//        playerDao.deleteMemberById("7");
 //    }
-
+//
     @Test
-    public void returnAllPlayerTest() {
-        PlayerDao playerDao = new PlayerDao();
-        List<Player> players = playerDao.returnAllPlayers();
+    public void getAllPlayersTest() {
+        List<Player> players = playerDao.getAllPlayers();
         System.out.println("Names:");
         for (Player player : players) {
             System.out.println(player.getName());
         }
     }
+    
+    @Test
+    public void getPlayerByIdTest() {
+        Player player = playerDao.getPlayerById("10");
+        System.out.println(player.getName());
+    }
+    
+//    @Test
+//    public void deletePlayerByIdTest() {
+//        playerDao.deletePlayerById("7");
+//    }
 
 }
