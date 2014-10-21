90ad93160c968ba2f089771825d4e8e9cdb00fad
Shubang <shubangsridhar@gmail.com>
2014-10-21 14:13:06 -0500

    Updates TeamTest class

--- src/main/java/edu/iastate/tests/TeamTests.java
+++ src/main/java/edu/iastate/tests/TeamTests.java
@@ -13,15 +13,6 @@ import edu.iastate.models.Team;
 import edu.iastate.models.Tournament;
 
 public class TeamTests {
-    @Test
-    public void returnAllPlayerTest() {
-        PlayerDao playerDao = new PlayerDao();
-        List<Player> players = playerDao.getAllPlayers();
-
-        TournamentDao tournamentdao = new TournamentDao();
-        Tournament tournament = tournamentdao.getTournamentById(1, false, false);
-        PlayerDao playerdao = new PlayerDao();
-    }
 
     @Test
     public void saveTeamTest() {
