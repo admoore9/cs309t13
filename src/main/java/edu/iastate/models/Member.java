3c7888a20c6cb2995dd9c7cf7f05c35bf46cd109
Andrew Moore <admoore9@gmail.com>
2014-10-21 12:16:09 -0500

    Add sex, height, and weight to member class

--- src/main/java/edu/iastate/models/Member.java
+++ src/main/java/edu/iastate/models/Member.java
@@ -15,46 +15,57 @@ import javax.persistence.InheritanceType;
  *
  */
 @Entity
-@Inheritance(strategy=InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Member {
 
     @Id
     @GeneratedValue
     @Column(name = "member_id")
-    private int member_id;  
-    
+    private int member_id;
+
     @Column(name = "name")
-    private String name;    
+    private String name;
 
     @Column(name = "username")
-    private String username; 
-    
+    private String username;
+
     @Column(name = "password")
-    private String password; 
-    
-    public enum UserType {PLAYER, OFFICIAL, ADMIN}; 
+    private String password;
+
+    public enum UserType {
+        PLAYER, OFFICIAL, ADMIN
+    };
 
     @Enumerated(EnumType.ORDINAL)
     @Column(name = "user_type")
     private UserType userType;
-    
-    public Member() {}
 
-    public Member(String name, String username, String password,
-            UserType userType) {
+    @Column(name = "sex")
+    private String sex;
+
+    @Column(name = "height")
+    private int height;
+
+    @Column(name = "weight")
+    private int weight;
+
+    public Member() {
+    }
+
+    public Member(String name, String username, String password, UserType userType) {
         this.name = name;
         this.username = username;
         this.password = password;
         this.userType = userType;
     }
-    
+
     public Member(String name, String username, String password) {
         this.name = name;
         this.username = username;
         this.password = password;
         this.userType = UserType.PLAYER;
-    }  
-    
+    }
+
     public UserType getUserType() {
         return userType;
     }
@@ -62,7 +73,7 @@ public class Member {
     public void setUserType(UserType userType) {
         this.userType = userType;
     }
-    
+
     public int getId() {
         return member_id;
     }
@@ -95,5 +106,27 @@ public class Member {
         this.password = password;
     }
 
-}
+    public String getSex() {
+        return sex;
+    }
 
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
