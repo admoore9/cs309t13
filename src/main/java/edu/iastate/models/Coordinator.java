fb4f6c9d062d0ed5ee920ba1650f073ae8d5fd0c
nawaf <nawafnaim@gmail.com>
2014-10-22 17:33:16 -0500

    added Coordinator class

---
+++ src/main/java/edu/iastate/models/Coordinator.java
@@ -0,0 +1,14 @@
+package edu.iastate.models;
+
+public class Coordinator extends Member {
+
+    public Coordinator() {
+        super();
+    }
+
+    public Coordinator(String name, String username, String password,
+            UserType userType) {
+        super(name, username, password, userType);
+    }
+
+}
