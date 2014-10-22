a5be0c643936ff7e2cd5a970ce1ed8d1021a43f9
nawaf <nawafnaim@gmail.com>
2014-10-22 17:21:39 -0500

    added constructors

--- src/main/java/edu/iastate/models/Official.java
+++ src/main/java/edu/iastate/models/Official.java
@@ -3,14 +3,21 @@ package edu.iastate.models;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Official")
 public class Official extends Member {
+
+    public Official() {
+        super();
+    }
+
+    public Official(String name, String username, String password) {
+        super(name, username, password,
+                UserType.OFFICIAL);
+    }
 
     @ManyToMany(mappedBy = "officials")
     private List<Game> games;
