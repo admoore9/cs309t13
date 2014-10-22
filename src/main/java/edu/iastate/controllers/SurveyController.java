6b1021e310d8c128da4c7c40b152ad9fe8569435
Andrew Moore <admoore9@gmail.com>
2014-10-22 16:23:23 -0500

    Add survey javascript, tsp, and controller files

---
+++ src/main/java/edu/iastate/controllers/SurveyController.java
@@ -0,0 +1,10 @@
+package edu.iastate.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/survey")
+public class SurveyController {
+
+}
