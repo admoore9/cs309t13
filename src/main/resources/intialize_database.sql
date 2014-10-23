f1d56867033a1dc785b14c2c92d1e05949f6c4f8
nawaf <nawafnaim@gmail.com>
2014-10-23 14:08:54 -0500

    removed deprecated columns

--- src/main/resources/intialize_database.sql
+++ src/main/resources/intialize_database.sql
@@ -13,8 +13,6 @@ CREATE TABLE IF NOT EXISTS `cs309t13`.`Member` (
   `name` VARCHAR(45) NULL,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
-  `is_admin` TINYINT(1) NULL,
-  `is_official` TINYINT(1) NULL,
   `user_type` int(4) NOT NULL,
   `sex` VARCHAR(1) NULL,
   `height` INT NULL,
