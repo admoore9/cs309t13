acb2d964101597d22cc257a7dccfd49e920d8941
nawafnaim <nawafnaim@gmail.com>
2014-10-29 18:45:07 -0500

    updated inheritance heirarchy

--- src/main/java/edu/iastate/models/Coordinator.java
+++ src/main/java/edu/iastate/models/Coordinator.java
@@ -1,12 +1,21 @@
 package edu.iastate.models;
 
-public class Coordinator extends Member {
+
+public class Coordinator extends Official {
 
     public Coordinator() {
         super(UserType.COORDINATOR);
     }
 
-    public Coordinator(String name, String username, String password,
+    public Coordinator(String name, String username, String password) {
+        super(name, username, password, UserType.COORDINATOR);
+    }
+    
+    protected Coordinator(UserType userType) {
+        super(userType);
+    }
+
+    protected Coordinator(String name, String username, String password,
             UserType userType) {
         super(name, username, password, userType);
     }
