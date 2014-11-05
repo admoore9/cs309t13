9fb3ccbc2885618160d4c2ba4737f97b3d79b8d1
Andrew Moore <admoore9@gmail.com>
2014-11-04 18:56:24 -0600

    Add JavaScriptf file for validation and sending requests to server using ajax

---
+++ src/main/webapp/resources/js/register.js
@@ -0,0 +1,58 @@
+$( document ).ready(function() {
+	$('#registerForm').bootstrapValidator({
+		message: 'This value is not valid',
+		fields: {
+			name: {
+				validators: {
+					notEmpty: {
+						message: 'Your name is required'
+					},
+					stringLength: {
+						max: 45,
+						message: 'Your name must be less than 46 characters long'
+					}
+				}
+			},
+			username: {
+				validators: {
+					notEmpty: {
+						message: 'A username is required'
+					},
+					stringLength: {
+						max: 45,
+						message: 'Your username must be less than 46 characters long'
+					},
+					remote: {
+						message: 'The username is not available',
+						url: '/register/available'
+					}
+				}
+			},
+			password: {
+				validators: {
+					notEmpty: {
+						message: 'A password is required'
+					},
+					stringLength: {
+						max: 45,
+						message: 'Your password must be less than 46 characters long'
+					}
+				}
+			}
+		}
+	})
+	.on('success.form.bv', function(e) {
+		// Stop form from submitting normally
+		e.preventDefault();
+		var url = "/register/submit";
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: $('#registerForm').serialize(),
+			success: function(result) {
+				$('#registerContainer').fadeOut(500);
+				$('#registerAlert').delay(500).fadeIn(500);
+			}
+		});
+	});
+});
