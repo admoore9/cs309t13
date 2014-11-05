bb74cff2d1256c460f66c4373f6d4b2458d66d76
nawafnaim <nawafnaim@gmail.com>
2014-11-04 22:58:50 -0600

    added player field and getters and setters

--- src/main/java/edu/iastate/models/Availability.java
+++ src/main/java/edu/iastate/models/Availability.java
@@ -2,13 +2,16 @@ package edu.iastate.models;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,13 +23,52 @@ public class Availability {
     @Column(name = "availability_id")
     private int availability_id;
     
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Player player;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "availability")
-    private ArrayList<Day> days;
+    private List<Day> days;
     
     public Availability() {
         // initialize days list
         days = new ArrayList<Day>(Arrays.asList(new Day("Monday"), new Day("Tuesday"), new Day("Wednesday"), new Day("Thursday"), new Day("Friday"), new Day("Saturday"), new Day("Sunday")));
         
+    }
+
+    /**
+     * @return the availability_id
+     */
+    public int getAvailability_id() {
+        return availability_id;
+    }
+
+    /**
+     * @return the days
+     */
+    public List<Day> getDays() {
+        return days;
+    }
+
+    /**
+     * @param days the days to set
+     */
+    public void setDays(List<Day> days) {
+        this.days = days;
+    }
+
+    /**
+     * @return the player
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * @param player the player to set
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
     }
     
 }
