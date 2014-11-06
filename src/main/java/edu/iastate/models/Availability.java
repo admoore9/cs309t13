4632a318b1e03273fd23f460488e64ecea68be59
nawafnaim <nawafnaim@gmail.com>
2014-11-05 22:21:17 -0600

    changed days from list to set and modified annotation

--- src/main/java/edu/iastate/models/Availability.java
+++ src/main/java/edu/iastate/models/Availability.java
@@ -1,7 +1,9 @@
 package edu.iastate.models;
 
 import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +13,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 @Entity
 @Table(name = "Availability")
@@ -25,8 +30,9 @@ public class Availability {
     @JoinColumn(name = "member_id")
     private Player player;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "availability")
-    private List<Day> days;
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @OneToMany(mappedBy = "availability")
+    private Set<Day> days;
     
     public Availability() {}
 
@@ -40,14 +46,14 @@ public class Availability {
     /**
      * @return the days
      */
-    public List<Day> getDays() {
+    public Set<Day> getDays() {
         return days;
     }
 
     /**
      * @param days the days to set
      */
-    public void setDays(List<Day> days) {
+    public void setDays(Set<Day> days) {
         this.days = days;
     }
 
