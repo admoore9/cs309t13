b70741527d26644508efe2583063624697404b50
nawafnaim <nawafnaim@gmail.com>
2014-11-05 18:58:53 -0600

    added getAvailabilityById

--- src/main/java/edu/iastate/dao/AvailabilityDao.java
+++ src/main/java/edu/iastate/dao/AvailabilityDao.java
@@ -3,6 +3,7 @@ package edu.iastate.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
 import edu.iastate.models.Availability;
 import edu.iastate.utils.EntityManagerFactorySingleton;
@@ -36,6 +37,20 @@ public class AvailabilityDao {
         
         transaction.commit();
         entityManager.close();
+    }
+
+    public Availability getAvailabilityById(int id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        TypedQuery<Availability> query = entityManager.createQuery("from Availability a where a.availability_id = :id", Availability.class);
+        query.setParameter("id", id);
+        Availability availability = query.getSingleResult();
+
+        transaction.commit();
+        entityManager.close();
+        return availability;
     }
 
 }
