fac19a1485c73367410e08ba8a206fba18f1f1bb
Andrew Moore <admoore9@gmail.com>
2014-11-07 09:32:22 -0600

    Undo changes to team controller

--- src/main/java/edu/iastate/controllers/TeamController.java
+++ src/main/java/edu/iastate/controllers/TeamController.java
@@ -1,14 +1,15 @@
 package edu.iastate.controllers;
 
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.iastate.dao.TeamDao;
-import edu.iastate.models.*;
 import edu.iastate.models.Player;
 import edu.iastate.models.Team;
 
@@ -17,11 +18,7 @@ import edu.iastate.models.Team;
 public class TeamController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getTeam(Model model, HttpSession session) {
-
-        if (session.getAttribute("member") == null) {
-            return "redirect:denied";
-        }
+    public String getTeam(Model model) {
 
         TeamDao teamdao = new TeamDao();
         Team team = teamdao.getTeamById(2, true, true);
