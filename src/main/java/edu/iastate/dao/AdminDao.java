bfc038e30c1717cf2275f2af942caa16bafe87ae
nawafnaim <nawafnaim@gmail.com>
2014-11-08 10:05:17 -0600

    added promote method

--- src/main/java/edu/iastate/dao/AdminDao.java
+++ src/main/java/edu/iastate/dao/AdminDao.java
@@ -1,22 +1,25 @@
 package edu.iastate.dao;
 
-import java.util.List;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.Query;
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 
+import org.hibernate.Transaction;
 import org.hibernate.jdbc.Work;
 
 import edu.iastate.models.Admin;
 import edu.iastate.models.Member;
+import edu.iastate.models.Member.UserType;
 
-public class AdminDao extends MemberDao {
+public class AdminDao extends CoordinatorDao {
 
     public AdminDao() {
         super();
@@ -46,42 +49,94 @@ public class AdminDao extends MemberDao {
      * @param id The id of the admin you wish to fetch
      * @return admin by id
      */
-    public Admin getAdminById(int id) {
+    public Admin getAdminById(int id) throws NoResultException {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
 
         TypedQuery<Admin> query = entityManager.createQuery("from Admin a where a.member_id = :id", Admin.class);
         query.setParameter("id", id);
+        try {
         Admin admin = query.getSingleResult();
 
         transaction.commit();
         entityManager.close();
         return admin;
+        } catch (NoResultException noResultException) {
+            return null;
+        }
     }
 
     public <T extends Member> void promote(final T member) {
+        String memberClassName = member.getClass().getSimpleName().toUpperCase();
+        String targetTable = member.getUserType().name();
+        int id = member.getId();
+
+        // return if the class of passed member is equal to its userType
+        if (memberClassName.equals(targetTable)) {
+            return;
+        }
+        // return if passed member has row in target table
+        if (hasRowInTargetTable(targetTable, id) != null)
+            return;
+
+        // statements
+        String playerSql = "insert into Player (member_id) values (" + id + ")";
+        String officialSql = "insert into Official (member_id) values (" + id + ")";
+        String coordinatorSql = "insert into Coordinator (member_id) values (" + id + ")";
+        String adminSql = "insert into Admin (member_id, current_view) values (" + id + "," + UserType.ADMIN.ordinal() + ")";
+
+        // add rows to tables
+        if (memberClassName.equals("MEMBER") && targetTable.equals("ADMIN"))
+            addRowsToTables(Arrays.asList(playerSql, officialSql, coordinatorSql, adminSql));
+        else if (memberClassName.equals("PLAYER") && targetTable.equals("ADMIN"))
+            addRowsToTables(Arrays.asList(officialSql, coordinatorSql, adminSql));
+        else if (memberClassName.equals("OFFICIAL") && targetTable.equals("ADMIN"))
+            addRowsToTables(Arrays.asList(coordinatorSql, adminSql));
+        else if (memberClassName.equals("COORDINATOR") && targetTable.equals("ADMIN"))
+            addRowsToTables(Arrays.asList(adminSql));
+        else if (memberClassName.equals("MEMBER") && targetTable.equals("COORDINATOR"))
+            addRowsToTables(Arrays.asList(playerSql, officialSql, coordinatorSql));
+        else if (memberClassName.equals("MEMBER") && targetTable.equals("OFFICIAL"))
+            addRowsToTables(Arrays.asList(playerSql, officialSql));
+        else if (memberClassName.equals("MEMBER") && targetTable.equals("PLAYER"))
+            addRowsToTables(Arrays.asList(playerSql));
+    }
+
+    private Member hasRowInTargetTable(String targetTable, int id) {
+        System.out.println(id);
+        Member existingMember = null;
+        if (targetTable.equals("PLAYER"))
+            existingMember = this.getPlayerById(id);
+        else if (targetTable.equals("OFFICIAL"))
+            existingMember = this.getOfficialById(id);
+        else if (targetTable.equals("COORDINATOR"))
+            existingMember = this.getCoordinatorById(id);
+        else if (targetTable.equals("ADMIN"))
+            existingMember = this.getAdminById(id);
+        return existingMember;
+    }
+
+    private void addRowsToTables(final List<String> sqls) {
         final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        final org.hibernate.Session sess = (org.hibernate.Session) entityManager.getDelegate();
         
-        org.hibernate.Session sess = (org.hibernate.Session) entityManager.getDelegate();
         sess.doWork(
                 new Work() {
                     public void execute(Connection connection) throws SQLException 
-                    { 
-                        String sql = "insert into Player (member_id) values (?)";
-                        PreparedStatement statement = connection.prepareStatement(sql);
-                         
-                        statement.setString(1, member.getId() + "");
-                        
-                        statement.addBatch();
-                        
-                        statement.executeBatch();
-                        statement.close();
+                    {
+                        for (String sql : sqls) {
+                            Transaction transaction = sess.beginTransaction();
+                            Statement statement = connection.createStatement();
+                            statement.addBatch(sql);
+                            statement.executeBatch();
+                            transaction.commit();
+                            statement.close();
+                        }
                         connection.close();
-                        entityManager.close();
                     }
                 }
             );
-        
+        entityManager.close();
     }
 }
