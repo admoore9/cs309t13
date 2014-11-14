5138af981243e277b5061470e1562a217f473cd6
nawafnaim <nawafnaim@gmail.com>
2014-11-13 19:26:46 -0600

    minor changes to test

--- src/test/java/tests/PeriodTests.java
+++ src/test/java/tests/PeriodTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -18,28 +19,34 @@ import edu.iastate.models.Player;
 public class PeriodTests {
     @Test
     public void constructorTest() {
-        
+
         PlayerDao playerDao = new PlayerDao();
         Player player = new Player("test1", "test1", "123");
         playerDao.savePlayer(player);
-        
+
         AvailabilityDao availabilityDao = new AvailabilityDao();
         Availability availability = new Availability();
         availability.setPlayer(player);
-        availabilityDao.saveAvailability(availability);
-        
+        Availability savedAvailability = availabilityDao
+                .saveAvailability(availability);
+
         DayDao dayDao = new DayDao();
-        List<Day> days = Arrays.asList(new Day("Monday"), new Day("Tuesday"), new Day("Wednesday"), new Day("Thursday"), new Day("Friday"));
+        List<Day> days = Arrays.asList(new Day("Monday"), new Day("Tuesday"),
+                new Day("Wednesday"), new Day("Thursday"), new Day("Friday"),
+                new Day("Saturday"), new Day("Sunday"));
         for (Day day : days)
-            day.setAvailability(availability);
-        dayDao.saveDays(days);
-        
+            day.setAvailability(savedAvailability);
+        List<Day> savedDays = dayDao.saveDays(days);
+
         PeriodDao periodDao = new PeriodDao();
-        for (Day day : days) {
+        for (Day day : savedDays) {
             Period period = new Period(Slot.NINE);
             period.setDay(day);
             periodDao.savePeriod(period);
+
+            System.out.println(period.getSlot());
+            System.out.println(Period.Slot.SIX);
+            System.out.println(period.getSlot() == Period.Slot.NINE);
         }
-        
     }
 }
