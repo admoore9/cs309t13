f2deb5be4061f5d30ca503d8f57104fab13229eb
nawafnaim <nawafnaim@gmail.com>
2014-11-18 17:53:40 -0600

    undid many changes

--- src/main/java/edu/iastate/controllers/TeamController.java
+++ src/main/java/edu/iastate/controllers/TeamController.java
@@ -46,7 +46,7 @@ public class TeamController {
 
         team.setTournament(tournament);
         team.setName(teamName);
-        team.setTeamLeader(teamLeader);
+//        team.setTeamLeader(teamLeader);
 
         teamDao.saveTeam(team);
     }
@@ -199,15 +199,15 @@ public class TeamController {
         return true;
     }
 
-//    @RequestMapping(value = "/{id}/players", method = RequestMethod.GET)
-//    public @ResponseBody List<Member> getPlayersForTeam(@PathVariable int id) {
-//        TeamDao teamDao = new TeamDao();
-//        Team team = teamDao.getTeamById(id, false, true, false);
-//        for (Member player : team.getPlayers()) {
-//            // Causing circular references... Should actually fix that
-//            player.setSurveys(null);
-//            player.setTeams(null);
-//        }
-//        return team.getPlayers();
-//    }
+    @RequestMapping(value = "/{id}/players", method = RequestMethod.GET)
+    public @ResponseBody List<Member> getPlayersForTeam(@PathVariable int id) {
+        TeamDao teamDao = new TeamDao();
+        Team team = teamDao.getTeamById(id, false, true, false);
+        for (Member player : team.getPlayers()) {
+            // Causing circular references... Should actually fix that
+            player.setSurveys(null);
+            player.setTeams(null);
+        }
+        return team.getPlayers();
+    }
 }
