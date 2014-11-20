030d1287dee7144bf0f66a1894a082d298a7e77e
Andrew Moore <admoore9@gmail.com>
2014-11-19 22:48:36 -0600

    Import stuffs

--- src/main/java/edu/iastate/controllers/SurveyController.java
+++ src/main/java/edu/iastate/controllers/SurveyController.java
@@ -4,10 +4,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.iastate.dao.MemberDao;
 import edu.iastate.dao.SurveyDao;
