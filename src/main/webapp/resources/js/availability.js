4b649782278166a93ce65450dd19f929792d46a4
nawafnaim <nawafnaim@gmail.com>
2014-11-20 13:34:04 -0600

    wired select all checkbox

--- src/main/webapp/resources/js/availability.js
+++ src/main/webapp/resources/js/availability.js
@@ -1,20 +1,14 @@
 $( document ).ready(function() {
 	
-	$('#myTable').DataTable({
+	$('#availabilityTable').DataTable({
         "paging":   false,
         "ordering": false,
         "info":     false,
         "filter":	false
 	});
 	
-    $('#submit').click( function(e) {
-    	var table = $('#myTable').DataTable();
-//        var data = table.$('input, select').serialize();
-//        alert(
-//            "The following data would have been submitted to the server: \n\n"+
-//            data
-//        );
-        
+    $('#update').click( function(e) {
+        var table = $('#availabilityTable').DataTable();
 		e.preventDefault();
 		var url = "/availability/submit";
 		$.ajax({
@@ -28,4 +22,31 @@ $( document ).ready(function() {
         
         return false;
     } );
+
+    resetSelectAll();
+
+    $('#selectall').click(function () {
+        $('.checkbox').prop('checked', isChecked('selectall'));
+    });
 });
+
+
+function isChecked(checkboxId) {
+    var id = '#' + checkboxId;
+    return $(id).is(":checked");
+}
+function resetSelectAll() {
+    // if all checkbox are selected, check the selectall checkbox
+    // and viceversa
+    if ($(".checkbox").length == $(".checkbox:checked").length) {
+        $("#selectall").attr("checked", "checked");
+    } else {
+        $("#selectall").removeAttr("checked");
+    }
+
+    if ($(".checkbox:checked").length > 0) {
+        $('#edit').attr("disabled", false);
+    } else {
+        $('#edit').attr("disabled", true);
+    }
+}
