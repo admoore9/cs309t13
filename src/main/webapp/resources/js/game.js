ba96bf4c0141deaa555a1ad09aa3b5912d6991f1
Shubang <shubangsridhar@gmail.com>
2014-11-30 21:31:37 -0600

    Removed commented out lines

--- src/main/webapp/resources/js/game.js
+++ src/main/webapp/resources/js/game.js
@@ -8,46 +8,4 @@ $(document).ready(function() {
         });
     });
 
-//    var GameById = function() {
-//        // Get the current state of this to prevent issues
-//        var self = this;
-//        self.updateForm = $('#update-game-form');
-//        self.tournamentName = $('#update-game-location-input');
-//        
-//
-//        // Causes new values to be fetched from the server when the input boxes value is changed.
-//        self.setInputHandler = function() {
-//            self.updateForm.on('submit', self.updateGame);
-//        };
-//
-////        self.getDataUpdateUI = function()  {
-////            var id = self.inputBox.val();
-////            $.get('/tournament/' + id, "", self.handleData, "json");
-////        };
-//
-////        self.handleData = function(response) {
-////            self.tournamentName.val(response.name);
-////            self.minPlayers.val(response.minPlayers);
-////            self.maxPlayers.val(response.maxPlayers);
-////            self.teamsPerGame.val(response.teamsPerGame);
-////            self.officialsPerGame.val(response.officialsPerGame);
-////        };
-//
-//        self.updateGame = function(event) {
-//            event.preventDefault();
-//            $.post('/game/1/update', {
-//                location: self.location.val(),
-//            });
-//        };
-//
-////        self.handleGoToTournament = function(event) {
-////            event.preventDefault();
-////            alert(window.location.href);
-////            window.location.href = '/tournament/' + self.inputBox.val() + '/view';
-////        };
-//    };
-//
-//    var gameById = new GameById();
-//    gameById.setInputHandler();
-
 });
