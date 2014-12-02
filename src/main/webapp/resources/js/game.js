80396378150f4dd024cd0e4e0bf407142e489d12
Shubang <shubangsridhar@gmail.com>
2014-12-01 18:12:51 -0600

    Redirect game and team page to refresh after submitting changes.

--- src/main/webapp/resources/js/game.js
+++ src/main/webapp/resources/js/game.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
             location: $('#update-game-location-input').val(),
             addOfficial: $('#update-official-add-input').val(),
             removeOfficial: $('#update-official-remove-input').val(),
+            success: function(result) {
+				window.location.href = "/game/" + gameId + "/view";
+			},
         });
     });
 
--- src/main/webapp/resources/js/team.js
+++ src/main/webapp/resources/js/team.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
             teamName: $('#update-team-name-input').val(),
             addPlayer: $('#update-player-add-input').val(),
             removePlayer: $('#update-player-remove-input').val(),
+            success: setTimeout(function(result) {
+				window.location.href = "/team/" + teamId + "/view";
+			}, 500),
         });
     });
 });
